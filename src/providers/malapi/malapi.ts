f6210abd3a1ec9c503ac5546442dc4ab44bb5f8a
Augusto <augusto@imd.ufrn.br>
2020-09-25 19:04:15 -0300

    add arquivos

---
+++ src/providers/malapi/malapi.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the MalapiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MalapiProvider {
+
+  apiUrl: String = "https://api.jikan.moe/v3";  
+  constructor(public http: HttpClient) {
+
+  }
+
+  getMangas(query: String) {
+    return new Promise((resolve, reject) => {
+        let url = this.apiUrl+'/search/manga?limit=5&q='+query;
+        console.log(url);
+        this.http.get(url)
+          .subscribe((result: any) => {
+            //resolve(result);
+            resolve(result.results.map(function(item){
+              return {
+                titulo: item.title,
+                imagem: item.image_url
+              }
+            }));
+            //resolve(result.results);
+          },
+          (error) => {
+            reject(error);
+          });
+      });
+  }
+
+}
